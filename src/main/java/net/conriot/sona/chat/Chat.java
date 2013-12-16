4346588d3c008eadfe9e7fa2240efd288f89b651
Endain <endain.gaming@gmail.com>
2013-12-15 22:17:01 -0800

    Fixed public API function by exposing them to the outside world.

--- src/main/java/net/conriot/sona/chat/Chat.java
+++ src/main/java/net/conriot/sona/chat/Chat.java
@@ -5,17 +5,17 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Chat extends JavaPlugin {
-	private ChatManager chat;
-	private MessageManager message;
+	private static ChatManager chat;
+	private static MessageManager message;
 	
 	@Override
 	public void onEnable() {
 		// Register the chat manager which will handle all chat
-		this.chat = new ChatManager(this);
+		chat = new ChatManager(this);
 		// Register the message manager which will handle all custom messages
-		this.message = new MessageManager();
+		message = new MessageManager();
 		// Register all basic economy commands for this manager
-		Commands c = new Commands(this.chat, this.message);
+		Commands c = new Commands(chat, message);
 		getCommand("msg").setExecutor(c);
 		getCommand("r").setExecutor(c);
 		getCommand("mute").setExecutor(c);
@@ -32,51 +32,51 @@ public class Chat extends JavaPlugin {
 		// Nothing to do here
 	}
 	
-	public String style(ColorScheme scheme, String input) {
-		return this.message.style(scheme, input);
+	public static String style(ColorScheme scheme, String input) {
+		return message.style(scheme, input);
 	}
 	
-	public void send(ColorScheme scheme, String input, Player player) {
-		this.message.send(scheme, input, player);
+	public static void send(ColorScheme scheme, String input, Player player) {
+		message.send(scheme, input, player);
 	}
 	
-	public void sendAll(ColorScheme scheme, String input) {
-		this.message.sendAll(scheme, input);
+	public static void sendAll(ColorScheme scheme, String input) {
+		message.sendAll(scheme, input);
 	}
 	
-	public void sendLocal(ColorScheme scheme, String input, Location loc, double range) {
-		this.message.sendLocal(scheme, input, loc, range);
+	public static void sendLocal(ColorScheme scheme, String input, Location loc, double range) {
+		message.sendLocal(scheme, input, loc, range);
 	}
 	
-	public boolean mute(String target, long duration) {
-		return this.mute(target, duration);
+	public static boolean mute(String target, long duration) {
+		return mute(target, duration);
 	}
 	
-	public boolean unmute(String target) {
-		return this.chat.unmute(target);
+	public static boolean unmute(String target) {
+		return chat.unmute(target);
 	}
 	
-	public void sendChannel(Player sender, String message, String channel) {
-		this.chat.sendChannel(sender, message, channel);
+	public static void sendChannel(Player sender, String message, String channel) {
+		chat.sendChannel(sender, message, channel);
 	}
 	
-	public void sendLocal(Player sender, String message, Location loc, double range) {
-		this.chat.sendLocal(sender, message, loc, range);
+	public static void sendLocal(Player sender, String message, Location loc, double range) {
+		chat.sendLocal(sender, message, loc, range);
 	}
 	
-	public void setChannel(Player target, String channel) {
-		this.chat.setChannel(target, channel);
+	public static void setChannel(Player target, String channel) {
+		chat.setChannel(target, channel);
 	}
 	
-	public void addChannel(Player target, String channel) {
-		this.chat.addChannel(target, channel);
+	public static void addChannel(Player target, String channel) {
+		chat.addChannel(target, channel);
 	}
 	
-	public void removeChannel(Player target, String channel) {
-		this.chat.removeChannel(target, channel);
+	public static void removeChannel(Player target, String channel) {
+		chat.removeChannel(target, channel);
 	}
 	
-	public void clearChannel(Player target, String channel) {
-		this.chat.clearChannel(target, channel);
+	public static void clearChannel(Player target, String channel) {
+		chat.clearChannel(target, channel);
 	}
 }
